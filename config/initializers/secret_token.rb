042b03e15e3b213c80b761cb3a511d916bc66a92
Kav Latiolais <kav@kav.la>
2011-12-03 10:03:09 -0800

    Moved the rails app to a rails folder. Really want to write node. So I'm going to.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-JigsawMembership::Application.config.secret_token = '545ffb0ef0b26eb7a9272bbe0e86a92d5d72d03581aa81d2d96c0cdd37c1156cb728705d101374133e925ca25c4dd133269c0f364506809efd2551cd184a3f20'
